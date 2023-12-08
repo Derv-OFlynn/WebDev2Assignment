6162257b1c47e4191f9ff369f98006eed09bb03a
derv <dervlaoflynn@gmail.com>
2023-12-08 08:13:09 +0000

    logout.php finalised

--- logout.php
+++ logout.php
@@ -11,9 +11,8 @@
         echo('<p style="color:red">Error:'.$_SESSION["error"]."</p>\n");
         unset($_SESSION["error"]);
     }
-    setcookie("cookiename", "cookievalue", time() - 3600);
     session_destroy();
-    setcookie("cookiename", "cookievalue", time() - 3600);
+    setcookie("cookiename", "cookievalue");
 ?>
 <!--
     Programmer: Derv O'Flynn
@@ -77,7 +76,7 @@
         <!--Contact Us block-->
         <div class="MainContainer">
             <div class="secondBorder">
-                <h2 class ="ContentHeading"> Online </h2>
+                <h2 class ="ContentHeading"> Contact Us </h2>
                 <p class = "ContainerParagraph">
 
                     <br> <br>
@@ -121,9 +120,6 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <!--Local Script-->
-        <script src="Scripts\coillteachlib.js"></script>
-        <!--End of scripts-->
     
     </body>
 
